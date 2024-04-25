2c8e8d35ec8a056a44d1f362e2fb1797cb01c735
t00218562k <you@example.com>
2024-04-25 11:56:19 +0100

    Apply code formatting corrections

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -25,7 +25,7 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import java.util.Locale;
 
 /**
- * PetClinic Spring Boot Application. dummy comment
+ * PetClinic Spring Boot Application. dummy comments
  *
  * @author Dave Syer
  *
